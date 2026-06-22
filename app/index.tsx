d645dc063fddf217e40ed1c3e6e2a73ccb99618c
vinayvyas05 <vyasvinay95@gmail.com>
2026-06-22 18:17:53 +0530

    setup nativewind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Redirect } from "expo-router";
 import { useAuthStore } from "@/store/auth.store";
+import "../global.css";
 
 export default function Index() {
   const { user, authLoading } = useAuthStore();
